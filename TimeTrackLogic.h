8d4f69c6b4f766e32d6546f6fdde0456ce0bfec2
tf2co <tf2coolguy431@gmail.com>
2026-01-02 08:38:36 +0900

    Revert "Implement CallViz and TimeTrack GUI Visualization"
    This reverts commit 6dad598f5f2fa0ffa9124fdd62b41d656fbbd02e.

--- TimeTrackLogic.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <map>
-#include <vector>
-#include <string>
-#include <DbgEng.h>
-#include "TraceRecord.h"
-
-// Shared logic from timetrack.cpp
-std::map<int, std::vector<TraceRecord>> _TimeTrack(IDebugClient* client, const std::string& arg);
